82cc93ef99c571a7856e11e62552f2ca7f22d4bc
abouvier <abouvier@student.42.fr>
2016-04-04 03:56:31 +0200

    add foobar2000-bin

---
+++ foobar2000-bin/foobar2000-bin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+bin=foobar2000
+opts=(/immediate)
+data="${XDG_DATA_HOME:-$HOME/.local/share}/$bin"
+run="${XDG_RUNTIME_DIR:-/tmp}/$bin"
+
+for dir in "$data" "$run" ; do
+	[[ ! -d "$dir" ]] && mkdir -p "$dir"
+done
+
+mountpoint "$run" -q || unionfs "$data=RW:/usr/share/$bin=RO" "$run"
+
+case "$1" in
+	--play|--pause|--playpause|--prev|--next|--rand|--stop|--exit|--show|--hide)
+		wine "$run/$bin.exe" "/${1:2}" ;;
+	--add) opts+=(/add) ;&
+	--) shift ;&
+	*) winepath -w0 -- "$@" | xargs -0 wine "$run/$bin.exe" "${opts[@]}"
+esac
+
+pidof "$bin.exe" > /dev/null || fusermount -u "$run"
