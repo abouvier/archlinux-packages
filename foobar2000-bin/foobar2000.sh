18e1616f23035ea910fceda3969a98f86524e766
Alexandre Bouvier <contact@amb.tf>
2021-08-25 13:50:08 +0200

    foobar2000-bin: update to 1.6.7

--- foobar2000-bin/foobar2000.sh
+++ foobar2000-bin/foobar2000.sh
@@ -4,18 +4,16 @@ opts=(/immediate)
 data=${XDG_DATA_HOME:-$HOME/.local/share}/$bin
 run=${XDG_RUNTIME_DIR:-/tmp}/$bin
 
-for dir in "$data" "$run" ; do
-	[[ ! -d $dir ]] && mkdir -p "$dir"
-done
+install -d "$data" "$run"
 
-mountpoint "$run" -q || unionfs "$data=RW:/usr/share/$bin=RO" "$run"
+mountpoint -q "$run" || unionfs -o noatime "$data=RW:/usr/share/$bin=RO" "$run"
 
 case $1 in
-	--pause|--playpause|--prev|--next|--rand|--stop|--exit|--show|--hide) ;&
+	--pause | --playpause | --prev | --next | --rand | --stop | --exit | --show | --hide) ;&
 	--play) wine "$run/$bin.exe" "/${1:2}" ;;
 	--add) opts+=(/add) ;&
 	--) shift ;&
-	*) winepath -w0 -- "$@" | xargs -0 wine "$run/$bin.exe" "${opts[@]}"
+	*) winepath -w0 -- "$@" | xargs -0 wine "$run/$bin.exe" "${opts[@]}" ;;
 esac
 
-pidof "$bin.exe" >/dev/null || fusermount -u "$run"
+pidof "$bin.exe" > /dev/null || fusermount -u -z "$run"
